05e170debf489ac86e0fb1bd6df312c32c3af92b
EveMonte <mrpresident.evmount@gmail.com>
2022-09-01 12:40:43 +0300

    Initial commit

---
+++ app/src/main/java/by/bstu/fit/savelev/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package by.bstu.fit.savelev.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firstapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
