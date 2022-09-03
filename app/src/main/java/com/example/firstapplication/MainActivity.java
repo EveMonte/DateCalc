3ac05aa3ca8944167994dbcc3d8def8557d2d08b
EveMonte <mrpresident.evmount@gmail.com>
2022-09-03 09:39:59 +0300

    Completed task

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.firstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.firstapplication.newPackage.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < 10; i++) {
+            Log.d("MainActivity", " counter =" + i);
+        }
+        getIdValue();
+    }
+
+    private void getIdValue() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
---
+++ app/src/main/java/com/example/firstapplication/newPackage/TextFunction.java
@@ -0,0 +1,8 @@
+package com.example.firstapplication.newPackage;
+
+public class TextFunction {
+    public String getValue(){
+        //TODO do something
+        return "Hello from the first project";
+    }
+}
